f36802cc5b9a3a83f49828aeeb4aa04b559bde87
Javetz <vincentmajavu@yahoo.com>
2019-06-24 13:47:22 +0200

    Add files via upload

---
+++ Worker/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RabbitWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var reciever = new Receive();
+            reciever.ReceiveMessage();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Worker/Receive.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Rabbit.Messages;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace RabbitWorker
+{
+    public class Receive
+    {
+        public void ReceiveMessage()
+        {
+            var factory = new ConnectionFactory() { HostName = "192.168.50.20" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+
+                    var deserialisedMessage = JsonConvert.DeserializeObject<RabbitMessageWrapper>(message);
+                    var wrappedMessageType = deserialisedMessage.MessageType;
+                    var wrappedDeserialisedMessage = JsonConvert.DeserializeObject(deserialisedMessage.JSonMessage, wrappedMessageType);
+
+                    int dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 1000);
+
+                    Console.WriteLine(" [x] Done");
+
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+                channel.BasicConsume(queue: "task_queue",
+                                     autoAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
